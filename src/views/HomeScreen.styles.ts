c4dbf964fcc5113dae4177b3a63be4a58bee2a54
FraBzz <francesca.bzz.bozzoli@gmail.com>
2024-06-03 15:39:11 +0200

    step3: modifiche grafiche

--- src/views/HomeScreen.styles.ts
+++ src/views/HomeScreen.styles.ts
@@ -3,6 +3,6 @@ import { Platform, StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: Platform.OS === 'android' ? 30 : 0,
+    marginTop: Platform.OS === 'android' ? 20 : 0,
   },
 });
--- src/views/HomeScreen.tsx
+++ src/views/HomeScreen.tsx
@@ -1,6 +1,8 @@
+import { StatusBar } from 'expo-status-bar';
 import React, { useRef } from 'react';
-import { View } from 'react-native';
+import { Platform, View } from 'react-native';
 import { WebView } from 'react-native-webview';
+import NavigationBar from '../components/NavigationBar';
 import { WEB_VIEW_URL } from '../config/constants';
 import { styles } from './HomeScreen.styles';
 
@@ -9,10 +11,20 @@ const HomeScreen = () => {
 
   return (
     <View style={styles.container}>
+      {/* <StatusBar barStyle="dark-content" /> */}
+      {Platform.OS === 'ios' ? (
+        <StatusBar style="light" translucent={false} />
+      ) : (
+        <StatusBar style="light" backgroundColor='#000000'/>
+      )}
       <WebView
         ref={webviewRef}
         style={{ flex: 1 }}
         source={{ uri: WEB_VIEW_URL }}
+      />
+      <NavigationBar
+        onBack={() => webviewRef.current?.goBack()}
+        onForward={() => webviewRef.current?.goForward()}
       />
     </View>
   );
